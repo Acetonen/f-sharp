5761e9b8a65eba5dcdc451a940299ae0298acfd8
Anton Kovalev <acetonen@gmail.com>
2020-12-20 16:23:07 +0300

    lesson five

---
+++ lesson_five.fs
@@ -0,0 +1,11 @@
+// 16.1
+let notDivisible (n, m) = m % n = 0
+
+// 16.2
+let prime n =
+    let rec find = function
+        | (n,1) | (n,0) -> true
+        | (n,m) when n % m = 0 -> false
+        | (n,m) -> find (n, m - 1)
+
+    find (n, n - 1)
