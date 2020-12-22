5c77fb675a679e6838bc445dc8565d42156c1735
Anton Kovalev <acetonen@gmail.com>
2020-12-22 20:22:30 +0300

    update lesson six

--- lesson_six.fs
+++ lesson_six.fs
@@ -8,11 +8,10 @@ let rec isIthChar (s: string, n, c) = s.[n] = c
 
 // 17.3
 let rec occFromIth (s, n, c) = 
-    let len = String.length s
-
     let rec count = function
-        | (sum,index) when index = len -> sum
+        | (sum,index) when index = String.length s -> sum
         | (sum,index) when s.[index] = c -> count (sum+1, index+1)
         | (sum,index) when s.[index] <> c -> count (sum, index+1)
+        | _ -> 0
     
     count (0, n)
