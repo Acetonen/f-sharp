f0a1a4ff75af3a85390ee47f7603c434173836c5
Anton Kovalev <acetonen@gmail.com>
2020-12-27 20:07:34 +0300

    lesson seven

---
+++ lesson_seven.fs
@@ -0,0 +1,13 @@
+// 20.3.1
+let vat n x = x * (float(n) / 100.00 + 1.0)
+
+// 20.3.2
+let unvat n x = x / (float(n) + 100.0) * 100.0
+
+// 20.3.3
+let rec min f = 
+    let rec inner = function
+        | (f, n) when f n <> 0 -> inner (f, n + 1)
+        | (f, n) -> n
+
+    inner (f, 1)
