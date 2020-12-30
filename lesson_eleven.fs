4f9736c6be1f2ade684b3c598955c69c68418842
Anton Kovalev <acetonen@gmail.com>
2020-12-30 15:03:54 +0300

    lesson eleven fix 2

--- lesson_eleven.fs
+++ lesson_eleven.fs
@@ -6,6 +6,6 @@ type TimeOfDay = { hours : int; minutes : int; f: F }
 
 let (.>.) x y = 
     match x, y with
-        | x, y when x.f > y.f -> true
-        | x, y when x.f < y.f -> false
-        | _ -> x > y
+    | x, y when x.f > y.f -> true
+    | x, y when x.f < y.f -> false
+    | _ -> x > y
