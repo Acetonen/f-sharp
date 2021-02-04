09e6f9ceeb30ddf84f5816c4553b14181e3570e1
Anton Kovalev <acetonen@gmail.com>
2021-02-04 09:09:14 +0300

    lesson seventeen

--- lesson_16.fs
+++ lesson_16.fs
@@ -10,3 +10,11 @@ let rec allSubsets arrayLength subsetSize =
                 yield! subsets (Set.remove e s) 
         ]
     subsets (set [1 .. arrayLength])
+
+
+// let allSubsets n k = 
+//      let rec subsets xs= 
+//          match xs with 
+//            | [] -> [[]] 
+//            | head::tail -> List.fold (fun head1 tail1 -> (head::tail1)::tail1::head1) [] (subsets tail) 
+//      Set.filter (fun xs -> Set.count xs = k) (Set.ofList (List.foldBack (fun head tail -> (Set.ofList head) :: tail) (subsets [1..n]) [] )) 
---
+++ lesson_17.fs
@@ -0,0 +1,11 @@
+module Seventeen
+
+
+// 43.3
+let try_find key m = 
+    let rec findInTuples = function
+        | [] -> None
+        | (head, value) :: _ when head = key -> Some value
+        |  _ :: tail -> findInTuples tail
+    
+    m |> Map.toList |> findInTuples
