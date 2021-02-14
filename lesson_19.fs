3cee40b219af4edbbc73e428f3cc7723310dd7f1
Anton Kovalev <acetonen@gmail.com>
2021-02-14 11:58:22 +0300

    lesson twenty

--- lesson_19.fs
+++ lesson_19.fs
@@ -10,6 +10,12 @@ let rec fibo1 n n1 n2 =
     | 0 -> n2
     | _ -> inner (1, n1, n2)
 
+// let rec fib1 n n1 n2 = 
+//     match n with
+//     | 0 -> n2
+//     | 1 -> n1
+//     | n -> fib1 (n - 1) (n1 + n2) n1
+    
 // 48.4.2
 let rec fibo2 n c =
     let rec inner = function
@@ -19,6 +25,12 @@ let rec fibo2 n c =
     match n with
     | 0 -> 0
     | _ -> inner (1, 1, 0)
+
+// let rec fib2 n c = 
+//     match n with
+//     | 0 -> c 0
+//     | 1 -> c 1
+//     | n -> fib2 (n - 2) (fun a -> fib2 (n - 1) (fun b -> c (a + b)))
 
 // 48.4.3
 let rec bigList n k =
---
+++ lesson_20.fs
@@ -0,0 +1,22 @@
+module Twenty
+
+// 49.5.1
+let even_seq = Seq.initInfinite (fun i -> i * 2)
+
+let rec fac = function
+    | 1 -> 1
+    | n -> n * fac(n-1)
+
+// 49.5.2
+let fac_seq = Seq.initInfinite (fun i -> if i = 0 then 1 else fac i)
+
+let seq n = 
+    let rec inner = function
+        | (current, count) when count = n -> current
+        | (current, count) when current < 0 -> inner (current * -1, count + 1)
+        | (current, count) -> inner ((current + 1) * -1, count + 1)
+    
+    inner (0, 0)
+
+// 49.5.3
+let seq_seq = Seq.initInfinite (fun i -> seq i)
