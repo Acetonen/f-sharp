b456f9bb7f5c62333cbaddd50e30b88b144de486
Anton Kovalev <acetonen@gmail.com>
2021-02-18 10:22:43 +0300

    lesson twenty one fix 2

--- lesson_21.fs
+++ lesson_21.fs
@@ -1,18 +1,18 @@
 module TwentyOne
 
 // 50.2.1
-let fac_seq = fun n -> seq {
+let fac_seq = Seq.initInfinite (fun n -> Seq.item n (seq {
     let current = ref 1
     yield !current
     for i in 1..n do
         current := !current * i
         yield !current
-}
+}))
 
 // 50.2.2
-let seq_seq = fun n -> seq {
+let seq_seq = Seq.initInfinite (fun n -> Seq.item n (seq {
     for i in 0..n do
         if i % 2 = 0 
         then yield i / 2 
         else yield (-1) * (i + 1)/2
-}
+}))
