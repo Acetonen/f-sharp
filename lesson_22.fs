21dffaac9c9109824557cc7a40e08d66ab26bf62
Anton Kovalev <acetonen@gmail.com>
2021-02-22 13:40:50 +0300

    lesson twenty two fix 1

--- lesson_22.fs
+++ lesson_22.fs
@@ -2,19 +2,15 @@ module TwentyTwo
 
 type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
 
-let rec nat (n:int) : 'a cell = Cons (n, lazy(nat(n+1)))
-
-let n0 = nat 0
-
 let hd (s : 'a cell) : 'a =
-    match s with
+  match s with
     Nil -> failwith "hd"
-    | Cons (x, _) -> x
+  | Cons (x, _) -> x
 
 let tl (s : 'a cell) : Lazy<'a cell> =
-    match s with
+  match s with
     Nil -> failwith "tl"
-    | Cons (_, g) -> g
+  | Cons (_, g) -> g
 
 
 // 51.3
